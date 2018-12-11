f4ab61895a1d5453ed55c44fb04fa44bcaa3784c
Developer <developer.web42@gmail.com>
2018-12-11 07:52:30 -0200

    11/12/2018 - Termos, Privacidade
    -

---
+++ SS/Controller/abigor/abigor.php
@@ -0,0 +1,103 @@
+<?php
+/*
+	{
+		"AUTHOR":"Matheus Maydana",
+		"CREATED_DATA": "10/12/2018",
+		"CONTROLADOR": "Abigor",
+		"LAST EDIT": "10/12/2018",
+		"VERSION":"0.0.1"
+	}
+*/
+class Abigor {
+
+	private $_cor;
+
+	private $_consulta;
+
+	private $_conexao;
+
+	private $_push = false;
+
+	private $_url;
+
+	private $_lang;
+
+	private $metas = array();
+
+	function __construct(){
+
+		$this->_cor = new Model_GOD;
+
+		$this->_conexao = new Model_Bancodados_Conexao;
+
+		$this->_consulta = new Model_Bancodados_Consultas($this->_conexao);
+
+		if(isset($_POST['push']) and $_POST['push'] == 'push'){
+			$this->_push = true;
+		}
+
+		$this->_url = $this->_cor->getUrl();
+
+		$this->_lang = $this->_cor->getLang();
+	}
+
+	function index(){
+
+		$mustache = array();
+		$this->metas['title'] = 'Abigor - fotos';
+
+		if($this->_push === false){
+
+			echo $this->_cor->_visao($this->_cor->_layout('abigor', 'abigor', $this->metas), $mustache);
+
+		}else{
+
+			echo $this->_cor->push('abigor', 'abigor', $mustache, $this->metas);
+		}
+	}
+
+	function termos(){
+
+		$mustache = array();
+		$this->metas['title'] = 'Abigor - fotos';
+
+		if($this->_push === false){
+
+			echo $this->_cor->_visao($this->_cor->_layout('abigor', 'termos', $this->metas), $mustache);
+
+		}else{
+
+			echo $this->_cor->push('abigor', 'termos', $mustache, $this->metas);
+		}
+	}
+
+	function privacidade(){
+
+		$mustache = array();
+		$this->metas['title'] = 'Abigor - fotos';
+
+		if($this->_push === false){
+
+			echo $this->_cor->_visao($this->_cor->_layout('abigor', 'privacidade', $this->metas), $mustache);
+
+		}else{
+
+			echo $this->_cor->push('abigor', 'privacidade', $mustache, $this->metas);
+		}
+	}
+
+	function regras(){
+
+		$mustache = array();
+		$this->metas['title'] = 'Abigor - fotos';
+
+		if($this->_push === false){
+
+			echo $this->_cor->_visao($this->_cor->_layout('abigor', 'regras', $this->metas), $mustache);
+
+		}else{
+
+			echo $this->_cor->push('abigor', 'regras', $mustache, $this->metas);
+		}
+	}
+}
